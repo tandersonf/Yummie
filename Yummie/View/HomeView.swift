b06d50161772498ed0f32f728fff3b334c504ce1
Anderson Oliveira <anderson.oliveira@cora.com.br>
2022-04-11 09:50:32 -0300

    setado projeto inicial

---
+++ Yummie/View/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  Yummie
+//
+//  Created by Cora on 11/04/22.
+//
+
+import Foundation
+import UIKit
+
+class HomeView: UIView {
+    
+    init(){
+        super.init(frame: .zero)
+        backgroundColor = .red
+    }
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
